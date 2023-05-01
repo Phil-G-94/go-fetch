356eaaf17468706d5de8820a7b4ed0937ee8a055
Phil Georgiou <philipposgeorgiou7@gmail.com>
2023-05-01 15:39:00 +0100

    basic UI, interactivity and logic complete

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/CartContext";
 
 export default function HeaderCartButton(props) {
+    const [btnIsActivated, setBtnIsActivated] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
     const numberOfCartItems = cartCtx.items.reduce((currentVal, item) => {
         return currentVal + item.amount;
     }, 0);
 
+    // .bump cart button animation
+
+    const { items } = cartCtx;
+
+    const btnClasses = `${classes.button} ${btnIsActivated ? classes.bump : " "}`;
+
+    useEffect(() => {
+        if (items.length === 0) return;
+        
+        setBtnIsActivated(true);
+
+        // animation timer: animation ceases 300ms after it fires   
+        const timer = setTimeout(() => {
+            setBtnIsActivated(false);
+        }, 300);
+
+        // cleanup function
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
